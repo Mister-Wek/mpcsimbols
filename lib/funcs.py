c15d1221de33ff11f92f6c45a15157b5736abccc
mrwek <68474288+Mister-Wek@users.noreply.github.com>
2024-04-15 17:02:46 +0300

    Create funcs.py

---
+++ lib/funcs.py
@@ -0,0 +1,22 @@
+prefix = '!s'
+def mpcsimbols_s(args):
+  return ' '
+
+def mpcsimbols_t(args):
+  return '	'
+
+def mpcsimbols_p(args):
+  return '§'
+
+def mpcsimbols_i(args):
+  return '∞'
+
+def mpcsimbols_lq(args):
+  return '«'
+
+def mpcsimbols_rq(args):
+  return '»'
+
+def mpcsimbols_c(args):
+  return '©'
+  
