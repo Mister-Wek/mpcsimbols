26e53c3166bb504cb102157a6e8c2878881179d9
mrwek <68474288+Mister-Wek@users.noreply.github.com>
2024-04-15 16:37:05 +0300

    Create meta.py

---
+++ lib/meta.py
@@ -0,0 +1,17 @@
+# Скачиваемые файлы
+files = ['funcs.py']
+
+# Короткое имя библиотеки
+name = 'mpcsimbols'
+
+# Версия библиотеки
+version = '0.0.0.1'
+
+# Авторство
+by = 'mrwek & weksoftware'
+
+# Описание
+description = 'Библиотека для использования некоторых символов'
+
+# Репозиторий на гитхаб
+github = 'Mister-Wek/mpcsimbols'
