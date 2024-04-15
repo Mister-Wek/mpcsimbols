b589d02f745812453764347a7b72021835c586df
mrwek <68474288+Mister-Wek@users.noreply.github.com>
2024-04-15 17:26:58 +0300

    Update funcs.py

--- lib/funcs.py
+++ lib/funcs.py
@@ -20,16 +20,16 @@ def mpcsimbols_rq(args):
 def mpcsimbols_c(args):
   return '©'
 
-def mpcsimbols_emoji(args):
-  emoji_en = {'duck':'🦆', 'tree':'🌳', 'palm_tree':'🌴', 'evergreen_tree':'🌲', 'snowflake':'❄️'}
-  emoji_ru = {'утка':'🦆', 'дерево':'🌳', 'пальма':'🌴', 'ёлка':'🌲', 'снежинка':'❄️'}
+def mpcsimbols_e(args):
+  e_en = {'duck':'🦆', 'tree':'🌳', 'palm_tree':'🌴', 'evergreen_tree':'🌲', 'snowflake':'❄️'}
+  e_ru = {'утка':'🦆', 'дерево':'🌳', 'пальма':'🌴', 'ёлка':'🌲', 'снежинка':'❄️'}
   r = ''
   for simbol in args:
     if simbol in emoji_en:
-      r += emoji_en[simbol]
+      r += e_en[simbol]
     elif simbol in emoji_ru:
-      r += emoji_ru[simbol]
+      r += e_ru[simbol]
   return r
 
-funcs = {'s':mpcsimbols_s, 't':mpcsimbols_t, 'p':mpcsimbols_p, 'i':mpcsimbols_i, 'lq':mpcsimbols_lq, 'rq':mpcsimbols_rq, 'c':mpcsimbols_c, 'emoji':mpcsimbols_emoji}
+funcs = {'s':mpcsimbols_s, 't':mpcsimbols_t, 'p':mpcsimbols_p, 'i':mpcsimbols_i, 'lq':mpcsimbols_lq, 'rq':mpcsimbols_rq, 'c':mpcsimbols_c, 'e':mpcsimbols_e}
   
